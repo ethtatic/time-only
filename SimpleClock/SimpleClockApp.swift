0ec754088e369ca57090a36750f04c795de5ff5b
Ethtatic <ethtatic@gmail.com>
2026-04-12 11:33:17 +0200

    Initial Commit

---
+++ SimpleClock/SimpleClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleClockApp.swift
+//  SimpleClock
+//
+//  Created by Lorenz Pennewiss on 12.04.26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleClockTests/SimpleClockTests.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleClockTests.swift
+//  SimpleClockTests
+//
+//  Created by Lorenz Pennewiss on 12.04.26.
+//
+
+import Testing
+@testable import SimpleClock
+
+struct SimpleClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
